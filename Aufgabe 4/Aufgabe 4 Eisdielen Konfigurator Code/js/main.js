e2497acfc8f7bbbdb4e49e157e7cf7e3acedde2a
Lisa Sanchez y Bittner <Lisa_Sanchez@web.de>
2019-04-24 15:13:27 +0200

    Verbesserung

--- Aufgabe 4/Aufgabe 4 Eisdielen Konfigurator Code/js/main.js
+++ Aufgabe 4/Aufgabe 4 Eisdielen Konfigurator Code/js/main.js
@@ -16,37 +16,36 @@ var a4;
                 fieldset.addEventListener("change", bestellUebersicht);
             }
         }
-        /*FUNKTION listet angewähltes auf und rechnet die Summe zusammen */
-        function bestellUebersicht(_event) {
-            let startSumme = 0;
-            let summe = 0;
-            let preis = 0;
-            let bestellEingabe = document.getElementsByTagName("input");
-            document.getElementById("zusammenfassung").innerHTML = "";
-            for (let i = 0; i < bestellEingabe.length; i++) {
-                if (bestellEingabe[i].checked == true) {
-                    let gesamtPreis = Number(bestellEingabe[i].value);
-                    startSumme += gesamtPreis;
-                    document.getElementById("summe").innerHTML = startSumme.toFixed(2).toString() + " " + "€";
-                    let bestellUebersicht = document.createElement("li");
-                    bestellUebersicht.innerHTML = `${bestellEingabe[i].id}`;
-                    document.getElementById("zusammenfassung").appendChild(bestellUebersicht);
-                }
+    }
+    /*FUNKTION listet angewähltes auf und rechnet die Summe zusammen */
+    function bestellUebersicht(_event) {
+        let startSumme = 0;
+        let summe = 0;
+        let preis = 0;
+        let bestellEingabe = document.getElementsByTagName("input");
+        document.getElementById("zusammenfassung").innerHTML = "";
+        for (let i = 0; i < bestellEingabe.length; i++) {
+            if ((bestellEingabe[i].name == "schokolade" && Number(bestellEingabe[i].value) > 0) || bestellEingabe[i].name == "vanille" && Number(bestellEingabe[i].value) > 0 || (bestellEingabe[i].name == "himbeere" && Number(bestellEingabe[i].value) > 0) || bestellEingabe[i].name == "himbeere" && Number(bestellEingabe[i].value) > 0 || bestellEingabe[i].name == "joghurt" && Number(bestellEingabe[i].value) > 0) {
+                preis = Number(bestellEingabe[i].value);
+                summe += preis;
+                console.log(summe);
+                let ziel = document.createElement("li");
+                ziel.innerHTML = `${bestellEingabe[i].value} Kugel ${bestellEingabe[i].name}, `;
+                document.getElementById("zusammenfassung").appendChild(ziel);
             }
-            for (let i = 0; i < bestellEingabe.length; i++)
-                // Nimmt Werte der Dropdown Boxen und zählt diese in die Übersicht
-                if ((bestellEingabe[i].name == "schokolade" && Number(bestellEingabe[i].value) > 0) || bestellEingabe[i].name == "vanille" && Number(bestellEingabe[i].value) > 0 || (bestellEingabe[i].name == "himbeere" && Number(bestellEingabe[i].value) > 0) || bestellEingabe[i].name == "himbeere" && Number(bestellEingabe[i].value) > 0 || bestellEingabe[i].name == "joghurt" && Number(bestellEingabe[i].value) > 0) {
-                    preis = Number(bestellEingabe[i].value);
-                    summe += preis;
-                    console.log(summe);
-                    let ziel = document.createElement("li");
-                    ziel.innerHTML = `${bestellEingabe[i].value} Kugel ${bestellEingabe[i].name}, `;
-                    document.getElementById("zusammenfassung").appendChild(ziel);
-                }
+            if (bestellEingabe[i].checked == true) {
+                let gesamtPreis = Number(bestellEingabe[i].value);
+                summe += gesamtPreis;
+                console.log(summe);
+                document.getElementById("summe").innerHTML = startSumme.toFixed(2).toString() + " " + "€";
+                let bestellUebersicht = document.createElement("li");
+                bestellUebersicht.innerHTML = `${bestellEingabe[i].id}`;
+                document.getElementById("zusammenfassung").appendChild(bestellUebersicht);
+            }
+            // Nimmt Werte der Dropdown Boxen und zählt diese in die Übersicht
             document.getElementById("summe").innerHTML = `Gesamtpreis:   ${summe} €`;
         }
     }
-    /* FUNKTION überprüft fehlende Angaben und gibt sie als Prompter aus*/
     function bestellungUeberpruefen(_event) {
         let kundenDaten = [];
         let kundenEingabe = document.getElementsByTagName("input");
--- Aufgabe 4/Aufgabe 4 Eisdielen Konfigurator Code/js/main.ts
+++ Aufgabe 4/Aufgabe 4 Eisdielen Konfigurator Code/js/main.ts
@@ -16,8 +16,9 @@ namespace a4 {
         let fieldsets: HTMLCollectionOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
 
         for (let i: number = 0; i < fieldsets.length; i++) {
-            let fieldset: HTMLFieldSetElement = fieldsets[i];{ 
-            fieldset.addEventListener("change", bestellUebersicht);
+            let fieldset: HTMLFieldSetElement = fieldsets[i]; {
+                fieldset.addEventListener("change", bestellUebersicht);
+            }
         }
     }
 
@@ -30,62 +31,52 @@ namespace a4 {
         let summe: number = 0;
         let preis: number = 0;
         let bestellEingabe: HTMLCollectionOf<HTMLInputElement> = document.getElementsByTagName("input");
-        document.getElementById("zusammenfassung").innerHTML = "";
-        for (let i: number = 0; i < bestellEingabe.length; i++) {
         
+        document.getElementById("zusammenfassung").innerHTML = "";
+
+        for (let i: number = 0; i < bestellEingabe.length; i++) {
+            if ((bestellEingabe[i].name == "schokolade" && Number(bestellEingabe[i].value) > 0) || bestellEingabe[i].name == "vanille" && Number(bestellEingabe[i].value) > 0 || (bestellEingabe[i].name == "himbeere" && Number(bestellEingabe[i].value) > 0) || bestellEingabe[i].name == "himbeere" && Number(bestellEingabe[i].value) > 0 || bestellEingabe[i].name == "joghurt" && Number(bestellEingabe[i].value) > 0) {
+                preis = Number(bestellEingabe[i].value);
+                summe += preis;
+                console.log(summe);
+                let ziel = document.createElement("li");
+                ziel.innerHTML = `${bestellEingabe[i].value} Kugel ${bestellEingabe[i].name}, `;
+                document.getElementById("zusammenfassung").appendChild(ziel);
+            }
 
             if (bestellEingabe[i].checked == true) {
                 let gesamtPreis: number = Number(bestellEingabe[i].value)
-                startSumme += gesamtPreis;
+                summe += gesamtPreis;
+                console.log(summe)
                 document.getElementById("summe").innerHTML = startSumme.toFixed(2).toString() + " " + "€";
                 let bestellUebersicht = document.createElement("li");
                 bestellUebersicht.innerHTML = `${bestellEingabe[i].id}`
                 document.getElementById("zusammenfassung").appendChild(bestellUebersicht)
             }
-        }
-        
-        for (let i: number = 0; i < bestellEingabe.length; i++)
-        
             // Nimmt Werte der Dropdown Boxen und zählt diese in die Übersicht
-            if ((bestellEingabe[i].name == "schokolade" && Number(bestellEingabe[i].value) > 0) || bestellEingabe[i].name == "vanille" && Number(bestellEingabe[i].value) > 0 || (bestellEingabe[i].name == "himbeere" && Number(bestellEingabe[i].value) > 0) || bestellEingabe[i].name == "himbeere" && Number(bestellEingabe[i].value) > 0 || bestellEingabe[i].name == "joghurt" && Number (bestellEingabe[i].value) > 0) {
-            preis = Number(bestellEingabe[i].value);
-            summe += preis;
-            console.log(summe);
-            let ziel = document.createElement("li");
-            ziel.innerHTML = `${bestellEingabe[i].value} Kugel ${bestellEingabe[i].name}, `;
-            document.getElementById("zusammenfassung").appendChild(ziel);
-        }
-        document.getElementById("summe").innerHTML = `Gesamtpreis:   ${summe} €`;
-    }
-}
 
-
-
-
-
-
-/* FUNKTION überprüft fehlende Angaben und gibt sie als Prompter aus*/
-
-function bestellungUeberpruefen(_event: Event): void {
-    let kundenDaten: string[] = [];
-    let kundenEingabe: HTMLCollectionOf<HTMLInputElement> = document.getElementsByTagName("input");
-    for (let i: number = 0; i < kundenEingabe.length; i++) {
-        if (kundenEingabe[i].value == "") {
-            let benoetigteDaten: string = kundenEingabe[i].name;
-            kundenDaten.push(benoetigteDaten);
-
+            document.getElementById("summe").innerHTML = `Gesamtpreis:   ${summe} €`;
         }
     }
 
-    if (kundenDaten.length == 0) {
-        alert("Vielen Dank für deine Bestellung");
+
+    function bestellungUeberpruefen(_event: Event): void {
+        let kundenDaten: string[] = [];
+        let kundenEingabe: HTMLCollectionOf<HTMLInputElement> = document.getElementsByTagName("input");
+        for (let i: number = 0; i < kundenEingabe.length; i++) {
+            if (kundenEingabe[i].value == "") {
+                let benoetigteDaten: string = kundenEingabe[i].name;
+                kundenDaten.push(benoetigteDaten);
+
+            }
+        }
+
+        if (kundenDaten.length == 0) {
+            alert("Vielen Dank für deine Bestellung");
+        }
+        else {
+            alert(`${kundenDaten} fehlt. Bitte noch ergänzen!`);
+        }
+
     }
-    else {
-        alert(`${kundenDaten} fehlt. Bitte noch ergänzen!`);
-    }
-
-}
-
-}
-
-
+}   
