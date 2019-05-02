701ffb4080cf0222df567eb03dd451c904371008
Lisa Sanchez y Bittner <Lisa_Sanchez@web.de>
2019-05-02 12:39:14 +0200

    Code Aufgabe 6 kopiert

---
+++ Aufgabe 6/Ausführbarer Code Aufgabe 6/js/verkäufer.ts
@@ -0,0 +1,48 @@
+/* Aufgabe 5: 
+Aufgabe: Aufgabe Nummer 5 - Eisdealer re-loaded
+Name: Lisa Sanchez y Bittner
+Matrikel: 260502 
+Datum: 28.04.2019
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.*/
+
+
+namespace a5 {
+
+    export interface Eisdaten {
+        name: string;
+        preis: number;
+        typ: string;
+
+    }
+        
+
+
+    export interface Fieldsetarray {
+        [datenArray: string]: Eisdaten[];
+    }
+
+    export let sortimentArray: Fieldsetarray = {
+        "Eissorten": [
+            { name: "Schokolade", preis: 1, typ: "number" },
+            { name:"Vanille", preis: 1, typ: "number" },
+            { name: "Himbeere", preis: 1, typ: "number" },
+            { name:"Joghurt", preis: 1, typ: "number" },
+
+        ],
+
+        "Behaelter": [
+            { name: "Waffel", preis: 0, typ: "radio" },
+            { name: "Becher", preis: 1, typ: "radio" }
+        ],
+
+        "Toppings": [
+            { name: "Streusel", preis: 1, typ: "number" },
+            { name: "Krokant", preis: 1, typ: "number" },
+            { name: "Schokosauce", preis: 1, typ: "number" },
+            { name: "Schlagsahne", preis: 1, typ: "number" }
+        ],
+
+
+    };
+}
+    
